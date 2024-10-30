d0e9996bc1f60bbac198edf42c19e6b4c8aff1a1
khromeengine <jianhengzhou003@gmail.com>
2024-10-29 18:21:59 -0700

    pos lerp finish
    tuning to be done later

--- Obscura/scripts/camera_controllers/position_lerp.gd
+++ Obscura/scripts/camera_controllers/position_lerp.gd
@@ -1,9 +1,10 @@
 class_name PositionLerp
 extends CameraControllerBase
 
-@export var follow_speed: float = 200
-@export var catchup_speed: float = 6
-@export var leash_distance: float = 14
+@export var follow_speed: float = 30
+@export var catchup_speed: float = 40
+@export var leash_distance: float = 8
+@export var catchup_slowdown_leash: float = 1
 
 var _speed: float = 2
 
@@ -27,15 +28,16 @@ func _process(delta) -> void:
 		var plane_target_dir = plane_dist_vector.normalized()
 		var target_dir = Vector3(plane_target_dir.x, 0 , plane_target_dir.y)
 		var target_spd = target.velocity.length()
+		var dist_ratio = clampf(xz_dist / leash_distance, 0, 1)
 		
-		if cplane.distance_to(tplane) > leash_distance:
-			_speed = target_spd
-		else:	
-			var speed_map = func(dist: float):
-				var new_speed = (dist / leash_distance)**2 * target_spd
-				return clampf(new_speed, catchup_speed, target_spd)
-			_speed = speed_map.call(xz_dist)
-		
+		if target_spd > 0:
+			if cplane.distance_to(tplane) > leash_distance:
+				_speed = target_spd
+			else:	
+				_speed = lerp(follow_speed, target_spd, dist_ratio)
+		else:
+			var catchup_dist_ratio = clampf(xz_dist / catchup_slowdown_leash, 0, 1)
+			_speed = lerp(catchup_speed, 0.0, 1.0 - catchup_dist_ratio)
 		global_position += _speed * target_dir * delta
 	else:
 		global_position.x = tpos.x
